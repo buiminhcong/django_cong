a27d90b51032e3dd574fc2a02a42b642c98f6027
NguyenHuuMung <nhm1248@gmail.com>
2022-04-19 00:03:15 +0700

    update CRUD kid clothes

--- clothes/urls.py
+++ clothes/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ClothesItemListAPIView, ClothesItemDetailAPIView
+
+from clothes import views
+from .views import ClothesItemListAPIView, ClothesItemDetailAPIView, CreateKidClothes, EditKidClothes
 
 app_name = "clothes"
 urlpatterns = [
     path('clothes_items/', ClothesItemListAPIView.as_view(), name="clothes_items"),
     path('clothes_items/<int:pk>/', ClothesItemDetailAPIView.as_view(), name="clothes_item_detail"),
-    path('edit_kidClothes/<int:pk>/', ClothesItemDetailAPIView.as_view(), name="edit_kidClothes"),
-    path('delete_kidClothes/<int:pk>/', ClothesItemDetailAPIView.as_view(), name="delete_kidClothes"),
+    path('edit_kidClothes/<int:pk>/', EditKidClothes.as_view(), name="edit_kidClothes"),
+    path('update_kidClothes/', EditKidClothes.as_view(), name="update_kidClothes"),
+    path('delete_kidClothes/<int:pk>/', views.delete, name="delete_kidClothes"),
+    path('add_kidClothes/', CreateKidClothes.as_view(), name="add_kidClothes"),
 ]
--- clothes/views.py
+++ clothes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from order.models import Cart
 from order.serializers import CartSerializer
-from .models import ClothesItem
+from .models import Clothes, ClothesItem, ClothesItemImage, KidClothes
 from .serializers import ClothesItemSerializer
 from django.shortcuts import render
 
@@ -57,5 +58,77 @@ class ClothesItemDetailAPIView(APIView):
 
         return render(request, 'detail.html', context)
         # return Response(serializer.data)
+def delete(request, pk):
+    kidClothes = KidClothes.objects.get(id=pk)
+    clothesItem = ClothesItem.objects.filter(clothes=kidClothes)
+    # clothesItemImages = ClothesItemImage.objects.filter(clothesItem=clothesItem).values_list('id', flat=True)
 
+    
+    kidClothes.delete()
+    clothesItem.delete()
+    # clothesItemImages.delete()
+    return redirect('http://127.0.0.1:8000/homeAdmin')
+class EditKidClothes(View):
+    def get(self, request, pk):
+        kidClothes = KidClothes.objects.get(id=pk)
+        kidClothesItem = ClothesItem.objects.get(clothes=kidClothes)
+        return render(request, 'clothes/editKidClothes.html', {'kidClothes': kidClothes, 'kidClothesItem': kidClothesItem})
 
+    def post(self, request):
+        id_kidClothesItem = request.POST['id_kidClothesItem']
+        kidClothesItem = ClothesItem.objects.get(id=id_kidClothesItem)
+        kidClothesItem.price = request.POST['price']
+        kidClothesItem.description = request.POST['description']
+        kidClothesItem.header = request.POST['header']
+        kidClothesItem.discount = request.POST['discount']
+        kidClothesItem.save()
+
+        id_kidClothes = request.POST['id_kidClothes']
+        kidClothes = KidClothes.objects.get(id=id_kidClothes)
+        kidClothes.productName = request.POST['productName']
+        kidClothes.material = request.POST['material']
+        kidClothes.countryOfOrigin = request.POST['countryOfOrigin']
+        
+        kidClothes.size = request.POST['size']
+        kidClothes.pattern = request.POST['pattern']
+        try:
+            tmp = request.POST['plusSize']
+            kidClothes.plusSize = True
+        except:
+            kidClothes.plusSize = False
+        kidClothes.brand = request.POST['brand']
+        kidClothes.gender = request.POST['gender']
+        kidClothes.recommendedAge = request.POST['recommendedAge']
+        kidClothes.save()
+
+        return redirect('http://127.0.0.1:8000/homeAdmin/#kidClothes')
+
+class CreateKidClothes(View):
+    def get(self, request):
+        return render(request, 'clothes/addKidClothes.html')
+
+    def post(self, request):
+        productName = request.POST['productName']
+        material = request.POST['material']
+        countryOfOrigin = request.POST['countryOfOrigin']
+        size = request.POST['size']
+        pattern = request.POST['pattern']
+        try:
+            plusSize = request.POST['plusSize']
+        except:
+            plusSize = False
+        brand = request.POST['brand']
+        gender = request.POST['gender']
+        recommendedAge = request.POST['recommendedAge']
+        kidClothes = KidClothes.objects.create(productName=productName, material=material, countryOfOrigin=countryOfOrigin, size=size,
+        pattern=pattern, plusSize=plusSize, brand=brand, gender=gender, recommendedAge=recommendedAge)
+        kidClothes.save()
+
+        price = request.POST['price']
+        description = request.POST['description']
+        header = request.POST['header']
+        discount = request.POST['discount']
+        clothesItem = ClothesItem.objects.create(price=price, description=description, header=header, discount=discount, clothes=kidClothes)
+        clothesItem.save()
+
+        return redirect('http://127.0.0.1:8000/homeAdmin/#kidClothes')
